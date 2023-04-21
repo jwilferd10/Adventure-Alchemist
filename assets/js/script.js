d99df6126e35e50611e12e8d29eeece89bb6c632
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-04-20 21:27:35 -0700

    Psuedocode added

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// Create a function that picks a number between 0-100 standardly
+
+    // Based on user input allow a user to customize the nubmer cap
+
+// Create an array that contains multiple dungeon themes
+
+// Create a function that randomly selects items in an array
+
+// Create an array that contains multiple monster types
+
+// Create a function that randomly selects at least 3 monster types
+
+// Randomly determine if traps are true/false
+
+// consider creating a basic loot system
