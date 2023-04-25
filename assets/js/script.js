a534a1a2b4d6854e4bae11768b5971da7aa5e383
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-04-24 17:53:42 -0700

    Modularized the codebase and added psuedo-code for a future update

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const textAreaEl = document.getElementById('textArea');
 // Dungeon Parameters
 const dungeonThemes = ['Ancient ruins', 'Caverns', 'Catacombs', 'Crypts', 'Dark forest', 'Desert tomb', 'Dwarf mine', 'Elemental plane', 'Ghost town', 'Goblin stronghold', 'Haunted mansion', 'Ice cave', 'Jungle temple', 'Labyrinth', 'Necropolis', 'Oceanic abyss', 'Orc stronghold', 'Palace dungeon', 'Sewer system', 'Underwater city', 'Vampire crypt', 'Wizard\'s tower', 'Zombie infested lab'];
 const monsterTypes = ['Undead', 'Abberations', 'Dragons', 'Constructs', 'Fiends', 'Giants', 'Fey', 'Elementals', 'Beasts', 'Humanoids'];
-const trapTypes = ["Pit trap", "Arrow trap", "Poison dart trap", "Swinging blade trap", "Net trap"];
+const trapTypes = ["Pit", "Arrow", "Poison dart", "Swinging blade", "Net"];
 const lootType = ["Gold coins", "Magic ring", "Potion of healing", "Scroll of fireball", "Jeweled sword"];
 
 // Array that collects all existing Dungeon Parameters 
@@ -15,32 +15,52 @@ const dungeonInfo = [
     lootType
 ];
 
-// Running a test to make sure the functionality works
-// TO-DO: Modularize the code afterwards
+// generate 
 const generateDungeon = () => {
-    const roomAmount = Math.floor(Math.random() * 10) + 1;
-    const theme = dungeonInfo[0][Math.floor(Math.random() * dungeonInfo[0].length)];
-    const monsters = dungeonInfo[1][Math.floor(Math.random() * dungeonInfo[1].length)];
-    const traps = dungeonInfo[2][Math.floor(Math.random() * dungeonInfo[2].length)];
-    const loot = dungeonInfo[3][Math.floor(Math.random() * dungeonInfo[3].length)];
+    // const roomAmount = Math.floor(Math.random() * 10) + 1;
+    const numberOfRooms = roomAmount();
+    const theme = generateTheme();
+    const monsters = generateMonsters();
+    const trap = setTrap();
+    const lootItem = setLoot();
 
-    console.log(`You have entered a ${theme} dungeon with ${roomAmount} rooms. Be careful of the ${monsters} and the ${traps} trap. You might find ${loot} if you're lucky!`);
+    console.log(`You have entered a ${theme} dungeon with ${numberOfRooms} rooms. Be careful of the ${monsters} and the ${trap} trap. You might find ${lootItem} if you're lucky!`);
 }
 
+// Randomly select room amount 
+const roomAmount = () => {
+    return Math.floor(Math.random() * 10) + 1;
+}
 
+// Randomly select theme
+const generateTheme = () => {
+    return dungeonInfo[0][Math.floor(Math.random() * dungeonInfo[0].length)];
+}
 
-// Create a function that picks a number between 0-100 standardly
+// Randomly select monster type
+const generateMonsters = () => {
+    return dungeonInfo[1][Math.floor(Math.random() * dungeonInfo[1].length)];
+}
 
-    // Based on user input allow a user to customize the nubmer cap
+// Randomly determine if traps are active in dungeon, then generate trap type
+const setTrap = () => {
+    return dungeonInfo[2][Math.floor(Math.random() * dungeonInfo[2].length)];
+}
 
-// Randomly select the theme of the dungeon 
+// Randomly select loot type
+const setLoot = () => {
+    return dungeonInfo[3][Math.floor(Math.random() * dungeonInfo[3].length)];
+}
 
-// const genContent = () => {
-//     let textTest = document.createElement('p');
-//     textTest.innerHTML = 'Hello world';
+// Create a 'previous searches' section that collects the generated code
 
-//     textAreaEl.append(textTest);
-// }
+// Use the {theme} of the title as the innerHTML name
 
+    // const genContent = () => {
+    //     let textTest = document.createElement('p');
+    //     textTest.innerHTML = 'Hello world';
+
+    //     textAreaEl.append(textTest);
+    // }
 
 generateBtnEl.addEventListener('click', generateDungeon);
