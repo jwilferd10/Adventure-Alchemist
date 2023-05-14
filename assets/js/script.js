cd9f54833e7c85f73076cb59c2612a10962fc404
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-05-13 23:56:19 -0700

    Interior/Exterior ambiance added, setAmbiance takes dungeonLocation as a parameter and checks if true or false to pass designated array data

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,8 +6,6 @@ const savedContentEl = document.getElementById('savedContent');
 const textAreaEl = document.getElementById('textArea');
 
 // Dungeon Parameters
-// const dungeonThemes = ['Ancient ruins', 'Battlefield', 'Caverns', 'Catacombs', 'Crypts', 'Dark forest', 'Desert tomb', 'Drow City', 'Dwarf mine', 'Elemental plane', 'Ghost town', 'Goblin stronghold', 'Haunted mansion', 'Ice cave', 'Jungle temple', 'Labyrinth', 'Necropolis', 'Oceanic abyss', 'Orc stronghold', 'Palace dungeon', 'Sewer system', 'Underwater city', 'Vampire crypt', 'Wizard\'s tower', 'Zombie infested lab'];
-
 const interiorDungeonThemes = ['Ancient ruins', 'Catacombs', 'Crypts', 'Drow City', 'Dwarf mine', 'Goblin stronghold', 'Haunted mansion', 'Labyrinth', 'Necropolis', 'Palace dungeon', 'Sewer system', 'Underwater city', 'Vampire crypt', 'Wizard\'s tower', 'Zombie infested lab'];
 const exteriorDungeonThemes = ['Battlefield', 'Caverns', 'Dark forest', 'Desert tomb', 'Elemental plane', 'Ghost town', 'Ice cave', 'Jungle temple', 'Oceanic abyss', 'Orc stronghold'];
 const monsterTypes = ['Undead', 'Abberations', 'Dragons', 'Constructs', 'Fiends', 'Giants', 'Fey', 'Elementals', 'Beasts', 'Humanoids'];
@@ -32,21 +30,57 @@ const lootType = [
     "an otherworldly artifact"
 ];
 
-const ambiance = [
+const interiorAmbiance = [
+    "Shadows twist and writhe on the walls like living things.",
+    "The air hums with a strange energy, making the hairs on the back of your neck stand up.",
+    "A thick silence hangs heavy in the air, broken only by the sound of your own footsteps.",
+    "A pervasive sense of unease settles over you like a heavy blanket.",
+    "The darkness is oppressive and seems to swallow sound.",
+    "The ground is spongy and moist, like walking on a soaked sponge.",
+    "The air is cool and crisp, with a hint of woodsmoke in the distance.",
+    "The ground is littered with small rocks and pebbles, crunching beneath your feet.",
+    "The air is heavy with the scent of damp earth and decaying leaves.",
+    "The sound of dripping water echoes through the chamber, adding to the sense of isolation.",
+    "The musty scent of age and decay fills your nostrils, making you want to cover your nose and mouth.",
+    "The area is suffused with a dim, flickering light that seems to come from nowhere.",
+    "The air feels thick and heavy, like you're wading through water.",
+    "The walls are covered in strange, arcane symbols that seem to pulse with a life of their own.",
+    "The silence is broken by a sudden creaking sound.",
+    "The only sound you can hear is the faint, steady beating of your own heart.",
+    "The area is filled with a low, buzzing hum that seems to vibrate through your bones.",
+    "The shadows seem to move and shift, playing tricks on your eyes and your mind.",
+    "The air is stale and stagnant, as if it hasn't been disturbed in years.",
+    "You feel as though you're being watched, even though there's no one there.",
+    "The area is so quiet you can hear the rustle of your clothes as you move.",
+    "The walls are made of a rough-hewn stone that seems to absorb the light.",
+    "The air is thick with the scent of incense and old, worn books.",
+    "The area is small and cramped, making you feel trapped and claustrophobic.",
+    "The space is filled with the sound of creaking floorboards, as if the area is alive.",
+    "The air is filled with the soft rustle of paper, as if someone is reading a book nearby.",
+    "The area is bathed in a sickly green light, casting everything in an eerie glow.",
+    "The ceiling is so high you feel as though you're standing in a cathedral.",
+];
+
+const exteriorAmbiance = [
     'The mist creeps in, cloaking everything in a ghostly haze.',
-    'Shadows twist and writhe on the walls like living things.',
-    'The air hums with a strange energy, making the hairs on the back of your neck stand up.',
-    'A thick silence hangs heavy in the air, broken only by the sound of your own footsteps.',
-    'A pervasive sense of unease settles over you like a heavy blanket.',
     'The world is quiet and the silence is loud.',
     'The air is thick and humid, like walking through a swamp.',
-    'The darkness is oppressive and seems to swallow sound.',
-    'The ground is spongy and moist, like walking on a soaked sponge.',
-    'The air is cool and crisp, with a hint of woodsmoke in the distance.',
     'The fog is thick and clings to everything like a damp veil.',
-    'The ground is littered with small rocks and pebbles, crunching beneath your feet.',
-    'The air is heavy with the scent of damp earth and decaying leaves.',
-    'The sky is a deep shade of blue, dotted with white fluffy clouds.'
+    'The sky is a deep shade of blue, dotted with white fluffy clouds.',
+    'The air is still, heavy with a sense of anticipation.',
+    'The sky is a murky gray, casting a somber tone over everything.',
+    'An eerie silence hangs over the landscape, broken only by the rustle of leaves.',
+    'The ground is slick with dew, making each step treacherous.',
+    'A thick mist rises from the ground, obscuring everything beyond a few feet.',
+    'A sense of foreboding settles over you, like a weight on your chest.',
+    'The wind is picking up, howling through the trees and stirring up leaves.',
+    'The air is thick with the scent of earth and wet stone.',
+    'The clouds hang low in the sky, casting everything in a dim, gray light.',
+    'The sound of rushing water echoes through the landscape, soothing and ominous at the same time.',
+    'The ground is soft and yielding, like walking on a sponge.',
+    'A sense of unease settles over you, as if you are being watched.',
+    'The landscape is barren and desolate, a testament to some long-forgotten disaster.',
+    'The sounds of battle can be heard all around, with swords clashing and arrows whistling through the air.',
 ];
 
 const areaDifficulty = [
@@ -69,7 +103,8 @@ const dungeonInfo = [
     monsterTypes,
     trapTypes,
     lootType,
-    ambiance,
+    exteriorAmbiance,
+    interiorAmbiance,
     areaDifficulty
 ];
 
@@ -430,10 +465,14 @@ const setTrap = () => {
 const setLoot = () => getRandomItem(dungeonInfo[4]);
 
 // setAmbiance
-const setAmbiance = () =>  getRandomItem(dungeonInfo[5]);
+const setAmbiance = (dungeonLocation) =>  { 
+    // Check to see if dungeonLocation is true or false and pass array data appropriately
+    let ambianceArray = dungeonLocation ? dungeonInfo[5] : dungeonInfo[6];
+    return getRandomItem(ambianceArray);
+}
 
 // set difficulty
-const setDifficulty = () => getRandomItem(dungeonInfo[6]);
+const setDifficulty = () => getRandomItem(dungeonInfo[7]);
 
 // format monsterStr
 const formatMonsterStr = (monstersArr) => {
