63bf6fdf0c178969bdd56e82a31b3d900e78c510
Jonathan Wilferd <jwilferd10@yahoo.com>
2023-08-11 18:35:12 -0700

    Created dungeon-parameters.js to hold and export dungeon info

---
+++ assets/js/dungeon-parameters.js
@@ -0,0 +1,92 @@
+// Dungeon Parameters
+export const interiorDungeonThemes = ['Abandoned Manor', 'Abyssal Fortress', 'Ancient ruins', 'Bandit Hideout', 'Besieged Fortress', 'Castle Ruins', 'Catacombs', 'Crypts', 'Drow City', 'Desert Tomb', 'Dwarf mine', 'Forsakened Cathedral', 'Goblin stronghold', 'Haunted mansion', 'Ice Cave', 'Jungle Temple', 'Labyrinth', 'Necropolis', 'Orc stronghold', 'Palace dungeon', 'Prison Riot', 'Sewer system', 'Set of Caverns', 'Sinister Asylum', 'Underwater city', 'Vampire crypt', 'Wizard\'s tower', 'Witch\'s Coven', 'Zombie infested lab'];
+export const exteriorDungeonThemes = ['Active Battlefield', 'Distant Battlefield', 'Elemental Plane', 'Forgotten Graveyard', 'Forest', 'Frozen Wasteland', 'Ghost Town', 'Graveyard', 'Jungle', 'Majestic Castle Grounds', 'Misty Mountains', 'Mysterious Island', 'Oceanic Abyss', 'Part of Hell', 'Part of the Abyss', 'Part of the Shadowplane', 'Part of the Underdark', 'Ruined Cityscape', 'Snowy Tundra', 'Volcanic Island', 'Windswept Desert'];
+export const monsterTypes = ['Undead', 'Abberations', 'Dragons', 'Constructs', 'Fiends', 'Giants', 'Fey', 'Elementals', 'Beasts', 'Humanoids'];
+export const trapTypes = ["Pit", "Arrow", "Poison Dart", "Swinging Blade", "Net", 'Frost', 'Fire', 'Acid', 'Electric', 'Explosive'];
+export const lootType = [
+    "a plethora of gold coins", 
+    "a single coin", 
+    "magical equipment", 
+    "a crate filled with potions of healing", 
+    "a book of offensive magic", 
+    "a book of defensive magic", 
+    "an ancient scroll", 
+    "a variety of gems", 
+    "an enchanted weapon", 
+    "a strange key",
+    "an ornate box containing unknown contents",
+    "a shimmering crystal orb",
+    "a small glowing statue",
+    "a strange deck of cards with unknown effects",
+    "an ancient tome with forbidden knowledge",
+    "an unusual wand with unknown properties",
+    "an otherworldly artifact"
+];
+
+export const interiorAmbiance = [
+    "Shadows twist and writhe on the walls like living things.",
+    "The air hums with a strange energy, making the hairs on the back of your neck stand up.",
+    "A thick silence hangs heavy in the air, broken only by the sound of your own footsteps.",
+    "A pervasive sense of unease settles over you like a heavy blanket.",
+    "The darkness is oppressive and seems to swallow sound.",
+    "The ground is spongy and moist, like walking on a soaked sponge.",
+    "The air is cool and crisp, with a hint of woodsmoke in the distance.",
+    "The ground is littered with small rocks and pebbles, crunching beneath your feet.",
+    "The air is heavy with the scent of damp earth and decaying leaves.",
+    "The sound of dripping water echoes through the chamber, adding to the sense of isolation.",
+    "The musty scent of age and decay fills your nostrils, making you want to cover your nose and mouth.",
+    "The area is suffused with a dim, flickering light that seems to come from nowhere.",
+    "The air feels thick and heavy, like you're wading through water.",
+    "The walls are covered in strange, arcane symbols that seem to pulse with a life of their own.",
+    "The silence is broken by a sudden creaking sound.",
+    "The only sound you can hear is the faint, steady beating of your own heart.",
+    "The area is filled with a low, buzzing hum that seems to vibrate through your bones.",
+    "The shadows seem to move and shift, playing tricks on your eyes and your mind.",
+    "The air is stale and stagnant, as if it hasn't been disturbed in years.",
+    "You feel as though you're being watched, even though there's no one there.",
+    "The area is so quiet you can hear the rustle of your clothes as you move.",
+    "The walls are made of a rough-hewn stone that seems to absorb the light.",
+    "The air is thick with the scent of incense and old, worn books.",
+    "The area is small and cramped, making you feel trapped and claustrophobic.",
+    "The space is filled with the sound of creaking floorboards, as if the area is alive.",
+    "The air is filled with the soft rustle of paper, as if someone is reading a book nearby.",
+    "The area is bathed in a sickly green light, casting everything in an eerie glow.",
+    "The ceiling is so high you feel as though you're standing in a cathedral.",
+];
+
+export const exteriorAmbiance = [
+    'The mist creeps in, cloaking everything in a ghostly haze.',
+    'The world is quiet and the silence is loud.',
+    'The air is thick and humid, like walking through a swamp.',
+    'The fog is thick and clings to everything like a damp veil.',
+    'The sky is a deep shade of blue, dotted with white fluffy clouds.',
+    'The air is still, heavy with a sense of anticipation.',
+    'The sky is a murky gray, casting a somber tone over everything.',
+    'An eerie silence hangs over the landscape, broken only by the rustle of leaves.',
+    'The ground is slick with dew, making each step treacherous.',
+    'A thick mist rises from the ground, obscuring everything beyond a few feet.',
+    'A sense of foreboding settles over you, like a weight on your chest.',
+    'The wind is picking up, howling through the trees and stirring up leaves.',
+    'The air is thick with the scent of earth and wet stone.',
+    'The clouds hang low in the sky, casting everything in a dim, gray light.',
+    'The sound of rushing water echoes through the landscape, soothing and ominous at the same time.',
+    'The ground is soft and yielding, like walking on a sponge.',
+    'A sense of unease settles over you, as if you are being watched.',
+    'The landscape is barren and desolate, a testament to some long-forgotten disaster.',
+    'The sounds of battle can be heard all around, with swords clashing and arrows whistling through the air.',
+    'Corpses new and old litter the land, death is in every corner.',
+    'Pervasive heat covers the land, engulfing everything in its scorching embrace.'
+];
+
+export const areaDifficulty = [
+    "This location looks like a cakewalk.",
+    "This location appears to be relatively simple, but don't let your guard down.",
+    "This location seems to have a moderate level of difficulty, so stay alert.",
+    "This location looks like it will require some skill and strategy to overcome.",
+    "This location appears to be a serious challenge that will test your abilities to the limit.",
+    "This location seems to be deceptively easy, so don't get overconfident.",
+    "This location appears to be filled with unexpected challenges, so be prepared for anything.",
+    "This location looks like it will be a tough but fair test of your abilities.",
+    "This location looks like it will require both strength and intelligence to overcome.",
+    "This location seems to be a true trial of your skills, so be ready to give it your all."
+];
